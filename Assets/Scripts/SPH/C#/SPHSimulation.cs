c1d9038820a7413dd68040871e4a8ce1c8637426
Caleb Deters <calebhd03@gmail.com>
2025-07-03 12:08:19 -0500

    hid parameters that aren't fully combined yet

--- Assets/Scripts/SPH/C#/SPHSimulation.cs
+++ Assets/Scripts/SPH/C#/SPHSimulation.cs
@@ -7,11 +7,11 @@ public class SPHSimulation : MonoBehaviour
 {
     [Header("Simulation Parameters")]
     public int maxParticles = 10000;
-    public float particleMass = 0.02f; // Mass of each particle
-    public float smoothingLength = 0.1f; // Smoothing length for the SPH kernel
-    public float restDensity = 1000f; // Rest density of the fluid
-    public float gasConstant = 2000f; // Gas constant for the equation of state
-    public float viscosityCoefficient = 0.01f; // Viscosity coefficient for the fluid
+    //public float particleMass = 0.02f; // Mass of each particle
+    //public float smoothingLength = 0.1f; // Smoothing length for the SPH kernel
+    //public float restDensity = 1000f; // Rest density of the fluid
+    //public float gasConstant = 2000f; // Gas constant for the equation of state
+    //public float viscosityCoefficient = 0.01f; // Viscosity coefficient for the fluid
     public float timeStep = 0.01f; // Time step for simulation updates
     public Vector3 boundarySize = new Vector3(10, 10, 10);
     public float smoothingRadius = 0.5f; 
